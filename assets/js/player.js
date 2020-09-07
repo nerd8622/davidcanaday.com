4ba76485c16bb9793f7b9195e2487da7634ec439
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-09-07 17:25:23 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,18 +1,16 @@
-function createPlayer(){
-  const gParams = new URLSearchParams(window.location.search);
-  if (gParams.has('id')){ytId = gParams.get('id')}
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/player_api"
-  var first = document.getElementsByTagName('link')[0];
-  first.parentNode.insertBefore(tag, first);
-  var player;
-  function onYoutubePlayerAPIReady() {
-    player = new YT.Player('player', {
-      height: '360',
-      width: '640',
-      videoId: ytId,
-      modestbranding: '1',
-      rel: '0'
-    });
-  }
+const gParams = new URLSearchParams(window.location.search);
+if (gParams.has('id')){ytId = gParams.get('id')}
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api"
+var first = document.getElementsByTagName('link')[0];
+first.parentNode.insertBefore(tag, first);
+var player;
+function onYoutubePlayerAPIReady() {
+  player = new YT.Player('player', {
+    height: '360',
+    width: '640',
+    videoId: ytId,
+    modestbranding: '1',
+    rel: '0'
+  });
 }
