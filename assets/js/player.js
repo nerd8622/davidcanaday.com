3082888ae1beef4dbbd7697e8c2c11cd9b995d69
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-09-07 16:35:36 -0600

    woah

---
+++ assets/js/player.js
@@ -0,0 +1,18 @@
+function createPlayer(){
+  conts gParams = new URLSearchParams(window.location.search);
+  if (gParams.has('id'){ytId = gParams.get('id')}
+  var tag = document.createElement('script');
+  tag.src = "https://www.youtube.com/player_api"
+  var first = document.getElementByTagName('link')[0];
+  first.parentNode.insertBefore(tag, first);
+  var player;
+  function onYoutubePlayerAPIReady() {
+    player = new YT.Player('ytplayer', {
+      height: '360',
+      width: '640',
+      videoId: ytId,
+      modestbranding: '1',
+      rel: '0'
+    });
+  }
+}
