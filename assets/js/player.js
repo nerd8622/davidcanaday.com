1ebe63bd1d548ffa751b05f120466155809b0661
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-09-07 17:54:56 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -2,10 +2,6 @@ function createPlayer(){
   const gParams = new URLSearchParams(window.location.search);
   //if (gParams.has('id')){ytId = gParams.get('id')}
   ytId = "hQfNtnKm5nA";
-  var tag = document.createElement('script');
-  tag.src = "https://www.youtube.com/player_api";
-  var first = document.getElementsByTagName('script')[0];
-  first.parentNode.insertBefore(tag, first);
   var player;
   function onYoutubePlayerAPIReady() {
     console.log("started player")
