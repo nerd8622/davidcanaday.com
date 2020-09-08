fb6ad2544fac934eda8db1341eb7866032589d98
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-09-08 09:20:01 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,3 +1,8 @@
 function createPlayer(){
+  var stag = document.createElement('script');
+  var ltag = document.createElement('link');
+  stag.src = "https://cdn.plyr.io/3.6.2/plyr.js";
+  ltag.rel = "stylesheet";
+  ltag.href = "https://cdn.plyr.io/3.6.2/plyr.css";
   const player = new Plyr('#player');
 }
