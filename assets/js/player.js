59e68a1148d4ef1e23463780b8593268e94c2058
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-09-08 09:15:18 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,16 +1,3 @@
 function createPlayer(){
-  // use id paramater from get request
-  const gParams = new URLSearchParams(window.location.search);
-  if (gParams.has('id')){id = gParams.get('id')}
-  // create the player
-  url = "https://www.youtube-nocookie.com/embed/" + id + "?controls=1&enablejsapi=1&modestbranding=1&showinfo=0&origin=https://www.davidcanaday.com&iv_load_policy=3&html5=1&fs=1&rel=0";
-  player = document.getElementById("ytplayer");
-  //fetch(url).then(res => {player.innerHTML = res;});
-  fetch(url).then(res => {testvar = res;});
-}
-function related(){
-  // get rid of the annoying related panel :)
-  document.getElementsByClassName('date-20200903')[0].getElementsByClassName('ytp-pause-overlay')[0];
-  annoying.parentElement.removeChild(annoying);
-  document.getElementById("ytplayer").onclick = null;
+  const player = new Plyr('#player');
 }
