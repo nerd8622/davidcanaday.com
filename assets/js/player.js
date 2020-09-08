04794f7d3b36efdc5ec77ab813aca4500a2e49e7
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-09-07 18:11:12 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,5 +1,5 @@
 function createPlayer(){
   id = "M7lc1UVf-VE";
   player = document.getElementById("ytplayer");
-  player.src = "https://www.youtube.com/embed/" + id + "?autoplay=1&origin=https://davidcanaday.com";
+  player.src = "https://www.youtube.com/embed/" + id + "?modestbranding=1&origin=https://davidcanaday.com&rel=1";
 }
