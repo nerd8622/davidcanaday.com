dc17f991b30bf8ea12da378570bd28eb40427e7d
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-09-07 18:06:36 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,4 +1,4 @@
-function createPlayer{
+function createPlayer(){
   id = "M7lc1UVf-VE";
   player = document.getElementById("ytplayer");
   player.src = "https://www.youtube.com/embed/" + id + "?autoplay=1&origin=https://davidcanaday.com";
