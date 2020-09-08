b609a8eab2f1d33081e030fc25c13d48e5ab52b8
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-09-07 18:02:24 -0600

    Create player.js

---
+++ assets/js/player.js
@@ -0,0 +1,5 @@
+function createPlayer{
+  id = "M7lc1UVf-VE";
+  player = document.getElementById("ytplayer");
+  player.src = "https://www.youtube.com/embed/" + id + "?autoplay=1&origin=https://davidcanaday.com";
+}
