b13ff13e228db64aff3f5bd33c301fae914de86b
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-09-08 10:43:17 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -3,7 +3,7 @@ function createPlayer(){
   var ltag = document.createElement('link');
   stag.src = "https://cdn.plyr.io/3.6.2/plyr.js";
   ltag.rel = "stylesheet";
-  ltag.href = "https://cdn.plyr.io/3.6.2/plyr.css";
+  ltag.href = "https://vjs.zencdn.net/7.8.4/video-js.css";
   document.head.insertBefore(stag, document.getElementsByTagName('script')[0]);
   document.head.insertBefore(ltag, document.getElementsByTagName('link')[0]);
   const player = new Plyr('#player');
