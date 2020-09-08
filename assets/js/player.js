da6646cb796e35428f7f4a3cd02e9b10f6e54c2f
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-09-07 21:21:20 -0600

    Update player.js

--- assets/js/player.js
+++ assets/js/player.js
@@ -2,5 +2,5 @@ function createPlayer(){
   const gParams = new URLSearchParams(window.location.search);
   if (gParams.has('id')){id = gParams.get('id')}
   player = document.getElementById("ytplayer");
-  player.src = "https://www.youtube.com/embed/" + id + "?modestbranding=1&origin=https://davidcanaday.com&rel=1";
+  player.src = "https://www.youtube-nocookie.com/embed/" + id + "?modestbranding=1&origin=https://davidcanaday.com&rel=0";
 }
