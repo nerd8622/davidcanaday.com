ae73ed182a8ad68128de0bc28cb162c02ebb84eb
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-09-23 21:50:21 -0600

    Update tiles.js

--- assets/js/tiles.js
+++ assets/js/tiles.js
@@ -2,7 +2,7 @@ function createTiles(file){
     file = file.split('\n');
     var x; var text; var img; var url; var image; var par; var button;
     for (x in file) {
-		if (x.length <= 3){console.log(x.length); continue;}
+		if (file[x].length <= 3){continue;}
 		var tile = document.createElement("DIV");
 		tile.className="content-item";
 		x = file[x].split(',');
