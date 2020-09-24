a053e3ede61a52a3ddfb11ab2bfd807304e18538
David Canaday <51459757+nerd8622@users.noreply.github.com>
2020-09-24 08:35:53 -0600

    Update index.js

--- widgets/line_motion/index.js
+++ widgets/line_motion/index.js
@@ -107,7 +107,7 @@ function graphIt(latex){
   //fix missing roots and extrema
   for (var i=1; i<=2; i++){
     for (var n=0; n<=3; n++){
-      if (frames[n].data[i].x.length <= 1){frames[n].data[i].x[0]=-15; frames[n].data[i].y[0]=0;}
+      if (frames[n].data[i].x.length < 1){frames[n].data[i].x[0]=-15; frames[n].data[i].y[0]=0;}
     }
   }
   
